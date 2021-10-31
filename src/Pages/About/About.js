dc6cdd15fce44be1bc93ce890c775e425208348f
rayhansheikh99 <rsheikh201031@msce.uiu.ac.bd>
2021-10-31 20:38:09 +0600

    about page added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
+import img from "../../images/slider/slider-4.jpg"
+import './about.css'
 
 const About = () => {
     return (
-        <div  className='page-size'>
-            <h4>Hajj Trip is a trusted and reliable tour and travel agency Dhaka  Division , Bangladesh . We are here to bring the luxury to your traveling. Hajj Trip is a proud member of Association of Travel Agents of Bangladesh-ATAB and also Tour Operators Association of Bangladesh. Safe traveling, your security, and your enjoyment are our main focus during your travels.</h4>
+
+
+        <div  className='about-page'>
+            <Row xs={1} md={2} className="g-4">
+
+                <Col>
+                <Card.Img className='' variant="top" src={img} />
+                </Col>
+                <Col>
+                <p>Hajj Trip is a trusted and reliable tour and travel agency Dhaka  Division , Bangladesh . We are here to bring the luxury to your traveling. Hajj Trip is a proud member of Association of Travel Agents of Bangladesh-ATAB and also Tour Operators Association of Bangladesh. Safe traveling, your security, and your enjoyment are our main focus during your travels.</p> 
+                </Col>
+                </Row>
+            {/* <img className='h-100 w-100' src={img} alt=""/><br/>
+            <p>Hajj Trip is a trusted and reliable tour and travel agency Dhaka  Division , Bangladesh . We are here to bring the luxury to your traveling. Hajj Trip is a proud member of Association of Travel Agents of Bangladesh-ATAB and also Tour Operators Association of Bangladesh. Safe traveling, your security, and your enjoyment are our main focus during your travels.</p> */}
+
         </div>
     );
 };
